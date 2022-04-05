b78d14d87a43e0b0ea91987e455597d3760c23b4
Maikel Golberto <92800007+MaikelGolberto@users.noreply.github.com>
2022-04-05 16:43:33 -0300

    Update Exercicio1Modularização.c

--- Exercicio 1/Exercicio1Modularização.c
+++ Exercicio 1/Exercicio1Modularização.c
@@ -1,15 +1,3 @@
-/*
-	Estruturas de Dados I - Turma: Ciência da Computação - Noturno 
-Número do Exercício: 1 - Modularização
-Nome do Arquivo: Exercicio1Modularização.c
-Número do Grupo: 01
-	Integrantes do Grupo:
-		Aluno: Danillo da Paz Cutrim				RGM/Matrícula: 27593886	
-		Aluno: João Gabriel Kreimer Torres			RGM/Matrícula: 28017188	
-		Aluno: Maikel Golberto Gontijo				RGM/Matrícula: 26837218   
-		Aluno: Robert Henrique Rodrigues Moreira	RGM/Matrícula: 26730421 
-*/
-
 #include<stdio.h>
 #include<locale.h>
 
@@ -21,29 +9,29 @@ int main(void) {
 	
 	setlocale(LC_ALL,"Portuguese");
 	
-	//Declaração de Variáveis
+	//DeclaraÃ§Ã£o de VariÃ¡veis
 	int num1, num2, num3, parImpar;
 	char continua;
 	
-	//Início do Programa
-	printf("Exercício 1\n\n");
+	//InÃ­cio do Programa
+	printf("ExercÃ­cio 1\n\n");
 	
 	do {
-		//Chama o procedimento Ler para o usúario digitar três números inteiros.
+		//Chama o procedimento Ler para o usÃºario digitar trÃªs nÃºmeros inteiros.
 		Ler(&num1, &num2, &num3); 
 		
-		//Chama o procedimento Ordenar para ordenar os números digitados pelo usúario em ordem crescente.
+		//Chama o procedimento Ordenar para ordenar os nÃºmeros digitados pelo usÃºario em ordem crescente.
 		Ordenar(&num1, &num2, &num3); 
 		
-		/* Atribui o resultado da função Par para a variável parImpar. Passando como paramêtro a variável num3 que é o maior número 
-		após o procedimento Ordenar. */
+		/* Atribui o resultado da funÃ§Ã£o Par para a variÃ¡vel parImpar. Passando como paramÃªtro a variÃ¡vel num3 que Ã© o maior nÃºmero 
+		apÃ³s o procedimento Ordenar. */
 		parImpar = Par(num3); 
 		
-		//Verifica se parImpar é verdadeiro ou falso, sendo que a função Par retorna 1 ou 0.
+		//Verifica se parImpar Ã© verdadeiro ou falso, sendo que a funÃ§Ã£o Par retorna 1 ou 0.
 		if (parImpar) { 
-			printf("Os números ordenados são: %d %d %d (par)", num1, num2, num3);
+			printf("Os nÃºmeros ordenados sÃ£o: %d %d %d (par)", num1, num2, num3);
 		} else { 
-			printf("Os números ordenados são: %d %d %d (ímpar)", num1, num2, num3);
+			printf("Os nÃºmeros ordenados sÃ£o: %d %d %d (Ã­mpar)", num1, num2, num3);
 		}
 		
 		printf("\nQuer continuar ? (S/N) ");
@@ -58,7 +46,7 @@ int main(void) {
 
 void Ler (int * n1, int * n2, int * n3){
 	
-	printf("Digite três números inteiros: ");
+	printf("Digite trÃªs nÃºmeros inteiros: ");
 	fflush(stdin);
 	scanf("%d %d %d", n1, n2, n3);
 }
@@ -69,29 +57,29 @@ void Ordenar (int * menor, int * meio, int * maior){
 	n2 = *meio,
 	n3 = *maior;
 	
-	if (n3 >= n1 && n3 >= n2) { //Compara o n3 com n1 e n2 para verificar se n3 é o maior, caso n3 seja o maior...
-		*maior = n3; 	//mantém n3 como maior
+	if (n3 >= n1 && n3 >= n2) { //Compara o n3 com n1 e n2 para verificar se n3 Ã© o maior, caso n3 seja o maior...
+		*maior = n3; 	//mantÃ©m n3 como maior
         if (n2 < n1) { 	//Verifica o meio e o menor
             *meio = n1;  //Caso o n2 seja menor do que o n1, n1 passa a ser o meio
             *menor = n2; //e o n2 passa a ser o menor
         } else {  //Caso o n2 seja maior que o n1 entra nesse else
-        	*meio = n2; //mantém n2 como meio
+        	*meio = n2; //mantÃ©m n2 como meio
         	*menor = n1; //e n1 como menor
 		}
-    } else if (n1 >= n2  && n1 >= n3) { //Compara o n1 com n2 e n3 para verificar se n1 é o maior, caso n1 seja o maior...
+    } else if (n1 >= n2  && n1 >= n3) { //Compara o n1 com n2 e n3 para verificar se n1 Ã© o maior, caso n1 seja o maior...
         *maior = n1;     //maior passa a ser o n1
         if (n2 > n3) {  //Verifica o meio e o menor
-        	*meio = n2;  //Caso o n2 seja maior do que o n3, mantém o n2 como meio
+        	*meio = n2;  //Caso o n2 seja maior do que o n3, mantÃ©m o n2 como meio
 			*menor = n3; //e n3 passa a ser o menor
         } else {   //Caso n2 seja menor do que o n3, entra nesse else
         	*meio = n3;  //meio passa a ser o n3
         	*menor = n2; //e menor passa a ser o n2
         }
-    } else if (n2 >= n1 && n2 >= n3) { //Compara o n2 com n1 e n3 para verificar se n2 é o maior, caso n2 seja o maior...
+    } else if (n2 >= n1 && n2 >= n3) { //Compara o n2 com n1 e n3 para verificar se n2 Ã© o maior, caso n2 seja o maior...
         *maior = n2;  //maior passa a ser o n2
 		if (n1 < n3) {  //Verifica o meio e o menor
         	*meio = n3;  //Caso o n3 seja maior do que o n1, n3 passa a ser o meio
-        	*menor = n1; //e mantém n1 como menor
+        	*menor = n1; //e mantÃ©m n1 como menor
     	} else {   //Caso o n1 seja maior do que o n3, entra nesse else
         	*meio = n1;  //o meio passa a ser n1
         	*menor = n3; //e n3 passa a ser o menor
